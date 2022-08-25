fbddfc344263029cbd3eac50c29d7ac69400068c
chen.yunhao <cyh19991002@gmail.com>
2022-08-25 17:23:31 +0800

    feat: 增加互联网备案信息

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -172,7 +172,7 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     footer: {
       createYear: 2020, // 博客创建年份
       copyrightInfo:
-        '干巴爹兔 | <a href="https://github.com/cloudhao1999/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
+        '互联网ICP备案： <a href="http://www.beian.miit.gov.cn/" target="_blank">闽ICP备18027236号</a>', // 博客版权信息、备案信息等，支持a标签或换行标签</br>
     },
 
     // 扩展自动生成frontmatter。（当md文件的frontmatter不存在相应的字段时将自动添加。不会覆盖已有的数据。）
